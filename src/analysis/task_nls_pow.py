198f1d605abd7293bc26572c3e36d0a1fa27375d
edoardofalchi <42804704+edoardofalchi@users.noreply.github.com>
2022-02-23 17:39:53 +0100

    Added function and task for noweight with power cost.

--- src/analysis/task_nls_pow.py
+++ src/analysis/task_nls_pow.py
@@ -7,6 +7,7 @@ from functools import partial
 
 #from src.config import BLD
 from src.analysis.benchmark_with_power import benchmark_power, benchmark_power_least_squares
+from src.analysis.weight_power import noweight_power
 
 
 # fix depends on 
@@ -18,8 +19,8 @@ def task_curve_fit_power(depends_on, produces):
     gamma_init_power, k_init_power, s_init_power =  19.8117987, 1.66306e-10, 7.74996
     k_scaler_power, s_scaler_power = 1e+57,1e+6
 
-    k_init_power = k_init_power/k_scaler_power
-    s_init_power = s_init_power/s_scaler_power
+    k_init_power /= k_scaler_power
+    s_init_power /= s_scaler_power
     
     st_values_power = [gamma_init_power, k_init_power, s_init_power]
 
@@ -49,8 +50,8 @@ def task_least_squares_power(depends_on, produces):
     gamma_init_power, k_init_power, s_init_power =  19.8117987, 1.66306e-10, 7.74996
     k_scaler_power, s_scaler_power = 1e+57,1e+6
 
-    k_init_power = k_init_power/k_scaler_power
-    s_init_power = s_init_power/s_scaler_power
+    k_init_power /= k_scaler_power
+    s_init_power /= s_scaler_power
     
     st_values_power = [gamma_init_power, k_init_power, s_init_power]
 
@@ -68,6 +69,47 @@ def task_least_squares_power(depends_on, produces):
     
 
     final = {'estimates' : bp52}
+
+    final = pd.DataFrame(final)
+
+    with open(produces, "w") as f:
+        final.to_csv(f, index=False)
+
+@pytask.mark.depends_on('../original_data/our_data.csv')
+@pytask.mark.produces('../analysis/noweight_opt_pow.csv')
+def task_noweight_power(depends_on, produces):
+    """Measure the runtime of pandas_batch_update and save the result."""
+
+    gamma_init_power, k_init_power, s_init_power =  19.8117987, 1.66306e-10, 7.74996
+    k_scaler_power, s_scaler_power = 1e+57,1e+6
+    k_init_power /= k_scaler_power
+    s_init_power /= s_scaler_power
+    st_values_power = [gamma_init_power, k_init_power, s_init_power]
+    alpha_init, a_init, beta_init, delta_init, gift_init = 0.003, 0.13, 1.16, 0.75, 5e-6
+    stvale_spec = [alpha_init, a_init, gift_init, beta_init, delta_init]
+    st_valuesnoweight_power = np.concatenate((st_values_power,stvale_spec)) # starting values
+
+    dt = pd.read_csv(depends_on)
+
+    args = {'payoff_per_100': dt.loc[dt['samplenw']==1].payoff_per_100,
+    'gift_dummy': dt.loc[dt['samplenw']==1].gift_dummy,
+    'delay_dummy': dt.loc[dt['samplenw']==1].delay_dummy,
+    'delay_wks': dt.loc[dt['samplenw']==1].delay_wks,
+    'payoff_charity_per_100': dt.loc[dt['samplenw']==1].payoff_charity_per_100,
+    'dummy_charity': dt.loc[dt['samplenw']==1].charity_dummy}
+    args = pd.DataFrame(args)
+
+    
+    sol = opt.curve_fit(noweight_power, 
+                    args,
+                    dt.loc[dt['samplenw']==1].logbuttonpresses_nearest_100,
+                    st_valuesnoweight_power)
+    bp53 = sol[0] 
+    sp53 = np.sqrt(np.diagonal(sol[1]))                     
+    
+
+    final = {'estimates' : bp53,
+            'variances': sp53}
 
     final = pd.DataFrame(final)
 
---
+++ src/analysis/weight_power.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+
+
+
+# Define the f(x,θ) to estimate all parameters but the probability weight in the power case
+
+def noweight_power(args, g, k, s, alpha, a, gift, beta, delta):
+    
+    pay100 = args['payoff_per_100']
+    gd = args['gift_dummy']
+    dd = args['delay_dummy']
+    dw = args['delay_wks']
+    paychar = args['payoff_charity_per_100']
+    dc = args['charity_dummy']
+    
+    check1= max(k, 1e-115)
+    check2= np.maximum(s + gift*0.4*gd + (beta**dd)*(delta**dw)*pay100 + alpha*paychar + a*0.01*dc, 1e-10)  
+    f_x = (-1/g * np.log(check1) + 1/g*np.log(check2))
+    
+    return f_x
+
+
+
+
