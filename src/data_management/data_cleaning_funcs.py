ad9f1177d7dd837420d4eac7ab1798d08b303641
nigar <s6nivali@uni-bonn.de>
2022-03-08 04:52:44 +0400

    Changed names of the variables, function etc.

--- src/data_management/data_cleaning_funcs.py
+++ src/data_management/data_cleaning_funcs.py
@@ -2,8 +2,9 @@ import numpy as np
 import pandas as pd
 
 
-def create_nls_data(data, treatment_classes, payoff_classes, treat_id_dummies):
+def create_nls_data(data, treatment_classes, payoff_classes, scenario_dummies):
     """Generate the dataset suitable for NLS by adding columns for payoffs and efforts to the original data.
+
     Args:
         data (dataset): initial data-mturk_clean_data_short
         treatment_classes (dict): dictionary containing treatment identifiers
@@ -12,19 +13,20 @@ def create_nls_data(data, treatment_classes, payoff_classes, treat_id_dummies):
  
     Returns:
         (pd.DataFrame): dataset ready to be used for NLS analysis
+    
     """
-
+    
     data_with_payoffs = _create_new_payoffs(data, treatment_classes, payoff_classes)
     data_with_efforts = _create_efforts(data)
     data.drop('buttonpresses', axis=1, inplace=True)
-    data_with_dummies =_create_dummy(data, treat_id_dummies)
-
+    data_with_dummies =_create_scenario_dummies(data, scenario_dummies)
 
     return pd.concat(objs=[data, data_with_payoffs, data_with_efforts, data_with_dummies], axis=1)
 
 
 def _create_new_payoffs(data, treatment_classes, payoff_classes):
     """Generate the dataset whose columns containt the treatments' payoffs 
+
     Args:
         data (dataset): initial data-mturk_clean_data_short
         treatment_classes (dict): dictionary containing treatment identifiers
@@ -33,6 +35,7 @@ def _create_new_payoffs(data, treatment_classes, payoff_classes):
  
     Returns:
         data_with_payoffs (pd.DataFrame): dataset with payoffs
+    
     """
     data_with_payoffs = data[['treatment']]
     
@@ -48,15 +51,17 @@ def _create_new_payoffs(data, treatment_classes, payoff_classes):
 
 def _create_efforts(data):
     """Generate the dataset whose columns containt the treatments' efforts 
+
     Args:
         data (dataset): initial data-mturk_clean_data_short
  
     Returns:
         data_with_efforts (pd.DataFrame): dataset with efforts
+    
     """
     data_with_efforts = data[['buttonpresses']]
     
-    data_with_efforts += 0.1 # python rounds 50 to 0, while stata to 100. by adding a small value we avoid this mismatch
+    data_with_efforts += 0.1
     data_with_efforts['buttonpresses_nearest_100'] = round(data_with_efforts,-2)
     data_with_efforts.loc[data_with_efforts['buttonpresses_nearest_100'] == 0, 'buttonpresses_nearest_100'] = 25
     data_with_efforts['logbuttonpresses_nearest_100']  = np.log(data_with_efforts['buttonpresses_nearest_100'])
@@ -64,17 +69,20 @@ def _create_efforts(data):
     return data_with_efforts
 
 
-def _create_dummy(data, treat):
+def _create_scenario_dummies(data, scenario_dummies):
+
     data_with_dummies = data[['treatment']]
-    for i in treat:
+    
+    for i in scenario_dummies:
         data_with_dummies[i] = 0
         if i == 'dummy1':
-            for j in treat[i]:
+            for j in scenario_dummies[i]:
                 data_with_dummies[i] += (data_with_dummies['treatment']==j).astype(int)
         else:
             data_with_dummies[i] = data_with_dummies['dummy1']
-            for j in treat[i]:
+            for j in scenario_dummies[i]:
                 data_with_dummies[i] += (data_with_dummies['treatment']==j).astype(int)
                 
-    data_with_dummies.drop('treatment', axis=1, inplace=True)            
+    data_with_dummies.drop('treatment', axis=1, inplace=True)
+
     return data_with_dummies
--- src/data_management/task_gen_nls_data.py
+++ src/data_management/task_gen_nls_data.py
@@ -10,6 +10,7 @@ from src.config import BLD
 from src.config import SRC
 from src.data_management.data_cleaning_funcs import create_nls_data
 
+
 treatment_classes = {
     'payoff_per_100' : ['1.1', '1.2', '1.3', '2', '1.4', '4.1', '4.2', '6.2', '6.1'],
     'payoff_charity_per_100' : ['3.1', '3.2'],
@@ -20,6 +21,7 @@ treatment_classes = {
     'weight_dummy' : ['6.1'],
     'gift_dummy' : ['10']
 }
+
 payoff_classes = {
     'payoff_per_100' : [0.01, 0.1, 0.0, 0.001, 0.04, 0.01, 0.01, 0.02, 1],
     'payoff_charity_per_100' : [0.01, 0.1],
@@ -30,7 +32,8 @@ payoff_classes = {
     'weight_dummy' : [1],
     'gift_dummy' : [1]
 }
-treat_id_dummies = {'dummy1':['1.1','1.2','1.3'],
+
+scenario_dummies = {'dummy1':['1.1','1.2','1.3'],
     'samplenw':['3.1','3.2','4.1','4.2','10'],
     'samplepr':['6.1','6.2']
 }
@@ -40,9 +43,9 @@ treat_id_dummies = {'dummy1':['1.1','1.2','1.3'],
 @pytask.mark.produces(BLD / "data" / "nls_data.csv")
 def task_generate_nls_data(depends_on, produces):
 
-    dt = pd.read_stata(depends_on)
+    raw_data = pd.read_stata(depends_on)
 
-    nls_data = create_nls_data(dt, treatment_classes, payoff_classes, treat_id_dummies)
+    nls_data = create_nls_data(raw_data, treatment_classes, payoff_classes, scenario_dummies)
     
     with open(produces, "w") as f:
         nls_data.to_csv(f, index=False)
