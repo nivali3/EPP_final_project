c3d6dda298ddd9f07a58f3d4e88f868537114d5e
nigar <s6nivali@uni-bonn.de>
2022-03-09 21:07:08 +0400

    Fixed typos in task that converts .csv into .tex

--- src/paper/task_gen_tex_tables.py
+++ src/paper/task_gen_tex_tables.py
@@ -6,6 +6,8 @@ import pandas as pd
 from os import listdir
 import pytask
 
+from pathlib import Path
+
 from src.config import BLD
 from src.config import SRC
 
@@ -13,11 +15,11 @@ from src.config import SRC
     "depends_on, produces",
     [
     (BLD / "tables" / f"{table}.csv", BLD / "paper" / f"{table}.tex")
-    for table in [file for file in listdir(BLD / "tables")]
+    for table in [Path(file).stem for file in listdir(BLD / "tables")]
     ],
 )
 def task_gen_tex_tables(depends_on, produces):
 
     table = pd.read_csv(depends_on)
-    with open(produces, "w") as tf:
+    with open(produces, "w", encoding="utf-8") as tf:
         tf.write(table.to_latex())
