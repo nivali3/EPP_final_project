060cade4716864ab1e622bcdcf7d53717778227c
nigar <s6nivali@uni-bonn.de>
2022-03-11 00:21:49 +0400

    Final adjustments to paper folder

--- src/paper/task_gen_tex_tables.py
+++ src/paper/task_gen_tex_tables.py
@@ -1,5 +1,6 @@
 """
 Converts <file>.csv into latex tables to be used as input in the final paper.
+
 """
 
 import pandas as pd
@@ -9,17 +10,22 @@ import pytask
 from pathlib import Path
 
 from src.config import BLD
-from src.config import SRC
+
 
 @pytask.mark.parametrize(
-    "depends_on, produces",
+    "depends_on, produces, table",
     [
-    (BLD / "tables" / f"{table}.csv", BLD / "paper" / f"{table}.tex")
+    (BLD / "tables" / f"{table}.csv", BLD / "paper" / f"{table}.tex", table)
     for table in [Path(file).stem for file in listdir(BLD / "tables")]
     ],
 )
-def task_gen_tex_tables(depends_on, produces):
+def task_gen_tex_tables(depends_on, produces, table):
 
-    table = pd.read_csv(depends_on)
+    if 'estimagic' in table:
+        tab = pd.read_csv(depends_on, header=[0,1], index_col=0)
+    else:
+        tab = pd.read_csv(depends_on, index_col=0)
+ 
     with open(produces, "w", encoding="utf-8") as tf:
-        tf.write(table.to_latex(na_rep="-"))
+        tf.write(tab.to_latex(na_rep="", bold_rows=True))
+        
